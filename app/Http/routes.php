7cb152cd34d973e5690616f0d8b42753ab443c67
Serkhio <pardinaserkhio@gmail.com>
2023-02-13 20:54:52 +0200

    create resource TaskController and route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('task', 'TaskController', [
+    'except'=> [
+        'show',
+    ]
+]);
