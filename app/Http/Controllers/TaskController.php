0b90a9fa14140aa7195a80e5ac13066260fcab51
Serkhio <pardinaserkhio@gmail.com>
2023-02-17 13:31:24 +0200

    tasks deleting

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -82,11 +82,13 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Task $task
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Task $task)
     {
-        //
+        $this->authorize('destroy', $task);
+        $task->delete();
+        return redirect()->route('task.index');
     }
 }
